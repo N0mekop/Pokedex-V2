f1960444a52a8efd7d7e5e469649b9254474a9dc
N0mekop <blinkevin@hotmail.com>
2023-02-15 16:01:44 +0100

    Ajout Routing et Homepage

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  title = 'Pokedex';
+  currentPokemonName = '';
+  pokemons: Pokemon[] = this.pokemonService.pokemons;
+
+  constructor(private pokemonService: PokemonService) {}
+
+  onAddPokemonClick() {
+    this.pokemonService.addPokemon(this.currentPokemonName);
+    this.currentPokemonName = '';
+  }
+
+  onDeletePokemonClick(index: number) {
+    this.pokemonService.deletePokemon(index);
+  }
+}
